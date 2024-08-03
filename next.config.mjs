fd5c60f4134ad0df628d08b5e53091439f5673ef
baramsivaramireddy <baramsivarami@gmail.com>
2024-08-03 22:14:02 +0530

    initial

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+    cacheOnFrontEndNav:true,
+    aggressiveFrontEndNavCaching:true,
+    reloadOnOnline:true,
+    swMinify:true,
+    disable:false,
+    workboxOptions:{
+        disableDevLogs:true
+    }
+});
+export default withPWA({
+    // Your Next.js config
+});
